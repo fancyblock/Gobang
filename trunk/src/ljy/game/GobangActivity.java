1558d23307f51639173ecb358067852957e5cca4
xujia1985@gmail.com <xujia1985@gmail.com@73f823cd-1a25-702e-ef9e-26e8bdfac634>
2012-02-04 17:13:08 +0000

    first changelist of this projcet

---
+++ trunk/src/ljy/game/GobangActivity.java
@@ -0,0 +1,13 @@
+package ljy.game;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GobangActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
